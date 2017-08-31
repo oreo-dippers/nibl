9b880669cd1cedf0395463d5ee7775199d909ed8
d-kang <david.kang714@gmail.com>
2017-08-31 08:38:48 -0700

    Init Webpack

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import ReactDOM form 'react-dom';
+//
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('root');
+// )
+
+console.log('hi worlds!')

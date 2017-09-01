484011273ef210c4d9530d9e0942466688a5e24c
d-kang <david.kang714@gmail.com>
2017-08-31 21:28:12 -0700

    Add Babel Loader to Webpack Config and index.ejs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-// import React from 'react';
-// import ReactDOM form 'react-dom';
-//
-// ReactDOM.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('root');
-// )
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, world!!</h1>,
+  document.getElementById('root')
+)
 
 console.log('hello from app.js!')
--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,25 @@ module.exports = {
     path: __dirname + '/dist',
     filename: 'app.bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Nibl',
+      template: 'index.ejs',
       minify: {
         collapseWhitespace: true
       },
       hash: true
-
     })
   ]
+
+
 }

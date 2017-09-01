cfc9435aac131b9f5db26a3d7a7e7f52f2a5656f
d-kang <david.kang714@gmail.com>
2017-09-01 08:15:46 -0700

    Add Font Loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,12 @@ module.exports = {
         }
       },
       {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"

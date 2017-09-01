259ef337b4fb92da4c8e6d13fe9b124abef5cb23
d-kang <david.kang714@gmail.com>
2017-09-01 15:43:47 -0700

    Init Browser Router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.scss'
 import App from './components/app.js'
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+
+var Router = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<Router />, document.getElementById('root'))
 
 console.log('hello from app.js!')

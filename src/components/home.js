26fc4d38a39cb40d1c6ab6a2a3857963bd9afce5
d-kang <david.kang714@gmail.com>
2017-09-02 12:30:54 -0700

    Home Has Welcome Message

--- src/components/home.js
+++ src/components/home.js
@@ -1,25 +1,9 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Header from './header';
-import Footer from './footer';
+import React from 'react'
 
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <br/>
-        <br/>
-        <br/>
-        Hi from Home Component
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <Footer />
-      </div>
-    )
-  }
-}
+const Home = () => (
+  <div>
+    <h1>Welcome to Nibl!</h1>
+  </div>
+)
 
-export default Home;
+export default Home

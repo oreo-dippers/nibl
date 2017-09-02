64ab5a88fab511d62723adf8ecce80c6657af4ad
d-kang <david.kang714@gmail.com>
2017-09-02 12:32:21 -0700

    Browser Router Renders App instead of Main

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './app.scss'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App';
 
 
-import Home from './components/home'
-import Dishes from './components/dishes'
-import Restaurants from './components/restaurants'
+var Router = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
 
-var Router = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={() => <Home />} />
-        <Route path='/dishes' component={() => <Dishes />} />
-        <Route path='/restaurants' component={() => <Restaurants />} />
-        <Route path='/restaurant/:name' component={() => <Restaurant/>} />
-      </Switch>
-    </BrowserRouter>
-  )
-}
-
-ReactDOM.render(<Router />, document.getElementById('root'))
-
-console.log('hello from app.js!')
+render(<Router />, document.getElementById('root'));

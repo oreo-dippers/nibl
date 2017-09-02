142f46b4b56fb2abf91aa231e2e20b5d5cadb403
d-kang <david.kang714@gmail.com>
2017-09-02 12:33:50 -0700

    Sample Restaurants API to Fetch Data From

---
+++ src/restaurants_api.js
@@ -0,0 +1,19 @@
+// A simple data API that will be used to get the data for our
+// components. 
+const RestaurantsAPI = {
+  restaurants: [
+    { store_id: 1, name: "Awesome_Tacos", location: "LA" },
+    { store_id: 2, name: "Awesome_Sushi", location: "San Jose" },
+    { store_id: 3, name: "Awesome_Spaghetti", location: "NYC" },
+    { store_id: 4, name: "Awesome_Burger", location: "Florida" },
+    { store_id: 5, name: "Awesome_Donuts", location: "Irvine" },
+    { store_id: 6, name: "Awesome_Pizza", location: "OC" }
+  ],
+  all: function() { return this.restaurants},
+  get: function(name) {
+    const isRestaurant = r => r.name === name
+    return this.restaurants.find(isRestaurant)
+  }
+}
+
+export default RestaurantsAPI

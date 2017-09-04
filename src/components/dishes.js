0e69783c29aa20d7b754857e8a6106ec435b6dae
d-kang <david.kang714@gmail.com>
2017-09-04 12:29:42 -0700

    Add An Input Field WIth Axios Call to Firbase Functions Endpoint

--- src/components/dishes.js
+++ src/components/dishes.js
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Form } from 'semantic-ui-react'
 
-const Dishes = () => (
-  <div>
-    <ul>
-      <li>Burger Dish</li>
-      <li>Taco Dish</li>
-      <li>Pizza Deep Dish</li>
-    </ul>
-  </div>
-)
 
-export default Dishes
+const host = 'https://us-central1-oreo-nibl.cloudfunctions.net/app'
+export default class Dishes extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      dishQuery: '',
+    };
+    this.submitDishQuery = this.submitDishQuery.bind(this);
+  }
+
+  submitDishQuery(e) {
+    e.preventDefault();
+    let dishQuery = this._dishes.value;
+    
+    this.setState({dishQuery});
+    console.log('dishQuery', dishQuery)
+    var data = {
+      data: dishQuery
+    }
+    axios.post('/test.json', data)
+      .then((res)=> {
+        console.log('res from /dishes', res)
+        console.log('res.data from /dishes', res.data)
+      })
+      .catch((err) => {
+        console.log('err from /dishes', err)
+      })
+
+  }
+
+
+  render() {
+    return (
+      <div>
+      
+        <Form onSubmit={this.submitDishQuery}>
+          <Form.Field>
+            <label>Find Dishes</label>
+            <input 
+              ref={(d)=>this._dishes = d}
+            />
+          </Form.Field>
+        </Form>
+        
+        You Searched for {this.state.dishQuery}
+      </div>
+    )
+  }
+};

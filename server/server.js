32e49e29dc4f3afbd511842abd95d0c54bf945ae
Ankit09osr <ankit09osr@gmail.com>
2017-09-04 17:26:07 -0700

    Add morgan for HTTP request log

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
+var logger = require('morgan')
 app.use(bodyParser.json())
+app.use(logger('dev'))
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.get('/foursquare', function (req, res) {
+  res.send('Hello foursquare')
 })
 
 app.listen(3000, function () {

a2d96947b57818621d9cb8e0bff8f9f1cfb5e397
Ankit09osr <ankit09osr@gmail.com>
2017-09-04 22:14:24 -0700

    Add test url for testing foursquare api

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const querystring = require('querystring');
+const foursquare = require('../foursquare-config/foursquare-config.js')
 var bodyParser = require('body-parser')
 var logger = require('morgan')
-var env = require('../foursquare-config/foursquare-config.js')
+
 
 app.use(bodyParser.json())
 app.use(logger('dev'))
@@ -12,9 +14,18 @@ app.get('/', function (req, res) {
 })
 
 app.get('/foursquare', function (req, res) {
-  // console.log('Env ', env.CLIENT_ID)
-  // console.log('Env2', env.CLIENT_SECRET)
-  res.send('Hello foursquare')
+  // console.log('Env ', foursquare.CLIENT_ID)
+  // console.log('Env2', foursquare.CLIENT_SECRET)
+  var url = 'https://api.foursquare.com/v2/venues/4bf9e1445ec320a11c028bd3';
+  var credentials = {
+    'v': '20170904'
+  };
+  credentials.client_id = foursquare.CLIENT_ID;
+  credentials.client_secret = foursquare.CLIENT_SECRET;
+  var qs = querystring.stringify(credentials);
+  var urlQuery = url+ '?' + qs;;
+  console.log('credentials', urlQuery);
+  res.send('Hello foursquare');
 })
 
 app.listen(3000, function () {

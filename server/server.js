c4146b9e3718fc34183853c12e0ba8cf530c8d1a
Ankit09osr <ankit09osr@gmail.com>
2017-09-05 08:10:08 -0700

    Modify server.js for separation of concern

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const foursquare = require('../foursquare-config/foursquare-config.js')
 var request = require('request')
 var bodyParser = require('body-parser')
 var logger = require('morgan')
-
+var routeHandler = require('./route-handler')
 
 app.use(bodyParser.json())
 app.use(logger('dev'))
@@ -14,28 +14,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.get('/foursquare', function (req, res) {
-  // console.log('Env ', foursquare.CLIENT_ID)
-  // console.log('Env2', foursquare.CLIENT_SECRET)
-  var url = 'https://api.foursquare.com/v2/venues/4bf9e1445ec320a11c028bd3';
-  var credentials = {
-    'v': '20170904'
-  };
-  credentials.client_id = foursquare.CLIENT_ID;
-  credentials.client_secret = foursquare.CLIENT_SECRET;
-  var qs = querystring.stringify(credentials);
-  var urlQuery = url+ '?' + qs;;
-  console.log('credentials', urlQuery);
+app.get('/venue',routeHandler.getVenue);
+app.get('/menu',routeHandler.getMenu);
 
-  request(urlQuery, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-    res.send('Hello foursquare');
-  });
-
-})
-
-app.listen(3000, function () {
+var port = 3000;
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })

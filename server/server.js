4877c6c12e10fa3472da84afe0d85063fe672a33
Ankit09osr <ankit09osr@gmail.com>
2017-09-04 22:32:17 -0700

    Add request to make http calls from server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const querystring = require('querystring');
 const foursquare = require('../foursquare-config/foursquare-config.js')
+var request = require('request')
 var bodyParser = require('body-parser')
 var logger = require('morgan')
 
@@ -25,7 +26,14 @@ app.get('/foursquare', function (req, res) {
   var qs = querystring.stringify(credentials);
   var urlQuery = url+ '?' + qs;;
   console.log('credentials', urlQuery);
-  res.send('Hello foursquare');
+
+  request(urlQuery, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.send('Hello foursquare');
+  });
+
 })
 
 app.listen(3000, function () {

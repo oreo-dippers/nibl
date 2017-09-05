7937374872a0b3f442d85e8bbb0db6ee2fc68794
Ankit09osr <ankit09osr@gmail.com>
2017-09-05 08:20:38 -0700

    Modify server.js for separation of concern

--- server/server.js
+++ server/server.js
@@ -1,23 +1,6 @@
-const express = require('express')
-const app = express()
-const querystring = require('querystring');
-const foursquare = require('../foursquare-config/foursquare-config.js')
-var request = require('request')
-var bodyParser = require('body-parser')
-var logger = require('morgan')
-var routeHandler = require('./route-handler')
+var app = require('./server-config');
 
-app.use(bodyParser.json())
-app.use(logger('dev'))
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.get('/venue',routeHandler.getVenue);
-app.get('/menu',routeHandler.getMenu);
-
-var port = 3000;
+var port = 3006;
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!')
+  console.log(`listening on http://localhost:${port}`)
 })

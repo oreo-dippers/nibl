f1f0260cbe3fcb90abdeb8ad4f3544f1038049cc
d-kang <david.kang714@gmail.com>
2017-09-06 06:54:56 -0700

    Use Props Passed Down from App Instead of Own Call to sample API

--- src/components/restaurantsList.js
+++ src/components/restaurantsList.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import RestaurantsAPI from '../restaurants_api'
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 // The RestaurantsList iterates over all of the restaurants and creates
 // a link to their profile page.
-const RestaurantsList = () => (
-  <div>
-    <ul>
-      {
-        RestaurantsAPI.all().map(r => (
-          <li key={r.store_id}>
-            <Link to={`/restaurants/${r.name}`}>{r.name}</Link>
-          </li>
-        ))
-      }
-    </ul>
-  </div>
-)
+class RestaurantsList extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          {
+            this.props.restaurants.map(r => (
+              <li key={r.store_id}>
+                <Link
+                  to={{
+                    pathname: `/restaurants/${r.name}`,
+                    state: { restaurant: r }
+                  }}
+                  >
+                  {r.name}
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
+}
 
 export default RestaurantsList

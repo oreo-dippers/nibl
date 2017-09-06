dc740a9e876a96286fb698034bac345e3df44f14
d-kang <david.kang714@gmail.com>
2017-09-06 06:52:36 -0700

    Mount Restaurant API State to App Component and Pass to Main

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Header from './header';
 import Main from './main';
 import Footer from './footer';
-
+import RestaurantsAPI from '../restaurants_api'
 import { Switch, Route } from 'react-router-dom'
 import Home from './home'
 
@@ -11,14 +11,21 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      test: 'testing state 123'
+      restaurants: [],
     }
   }
+
+  componentDidMount() {
+    var restaurants = RestaurantsAPI.restaurants
+    this.setState({restaurants});
+  }
+
   render() {
+    const { restaurants } = this.state;
     return (
       <div>
         <Header />
-        <Main test={this.state.test}/>
+        <Main restaurants={restaurants}/>
         <Footer />
       </div>
     )

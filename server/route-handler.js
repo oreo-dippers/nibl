46ea0740bafd2c60901fc7bc3e58de752e362550
Ankit09osr <ankit09osr@gmail.com>
2017-09-07 21:02:01 -0700

    Refactor getVenue()

--- server/route-handler.js
+++ server/route-handler.js
@@ -12,21 +12,20 @@ var bodyParser = require('body-parser')
 
 module.exports.getVenue = function(req, res) {
   var url = 'https://api.foursquare.com/v2/venues/4bf9e1445ec320a11c028bd3';
-  var credentials = {
-    'v': '20170904'
-  };
-  credentials.client_id = foursquare.CLIENT_ID;
-  credentials.client_secret = foursquare.CLIENT_SECRET;
-  var qs = querystring.stringify(credentials);
   var urlQuery = url+ '?' + qs;
-  console.log('Query String : ', urlQuery);
 
   request(urlQuery, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    var x = JSON.parse(body);
-    console.log('body:', x.response.venue);
-    res.send('Hello foursquare venue');
+    // Print the error if one occurred
+    if(error) {
+      console.log('error:', error);
+    }
+
+    // Print the response status code if a response was received
+    if(response) {
+      console.log('statusCode:', response && response.statusCode);
+    }
+    var data = JSON.parse(body);
+    res.send(data.response.venue);
   });
 
 };

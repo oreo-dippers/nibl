34c066bd56006ecd90f4e9db9a48930207562de9
Ankit09osr <ankit09osr@gmail.com>
2017-09-08 10:45:01 -0700

    Refactor getMenu() for separation of concern

--- server/route-handler.js
+++ server/route-handler.js
@@ -1,6 +1,5 @@
 const querystring = require('querystring')
 const foursquare = require('../api-config/foursquare-config.js')
-const request = require('request')
 const bodyParser = require('body-parser')
 const utils = require('./util')
 
@@ -9,11 +8,11 @@ var credentials = {
 };
 credentials.client_id = foursquare.CLIENT_ID;
 credentials.client_secret = foursquare.CLIENT_SECRET;
-var qs = querystring.stringify(credentials);
+const qs = querystring.stringify(credentials);
 
 module.exports.getVenue = function(req, res) {
-  var url = 'https://api.foursquare.com/v2/venues/4bf9e1445ec320a11c028bd3';
-  var urlQuery = url+ '?' + qs;
+  const url = 'https://api.foursquare.com/v2/venues/4bf9e1445ec320a11c028bd3';
+  const urlQuery = url+ '?' + qs;
   utils.apiCall(urlQuery, function(data) {
     res.send(data.response.venue);
   });
@@ -22,26 +21,15 @@ module.exports.getVenue = function(req, res) {
 
 
 module.exports.getMenu = function(req, res) {
-  var url = 'https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3/menu';
-  var urlQuery = url+ '?' + qs;
-
-  request(urlQuery, function (error, response, body) {
-    // Print the error if one occurred
-    if(error) {
-      console.log('error:', error);
-    }
-
-    // Print the response status code if a response was received
-    if(response) {
-      console.log('statusCode:', response && response.statusCode);
-    }
-
-    res.send(JSON.parse(body));
+  const url = 'https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3/menu';
+  const urlQuery = url+ '?' + qs;
+  utils.apiCall(urlQuery, function(data) {
+    res.send(data);
   });
 };
 
 module.exports.getRestaurants = function(req, res) {
-  var url = 'https://api.foursquare.com/v2/venues/explore';
+  const url = 'https://api.foursquare.com/v2/venues/explore';
   var myQuery = req.query;
   var parameterObj = {
     'venuePhotos': '1',
@@ -49,7 +37,7 @@ module.exports.getRestaurants = function(req, res) {
   parameterObj.query = myQuery.query;
   parameterObj.near = myQuery.near;
   parameterObj.radius = myQuery.radius;
-  var parameter = querystring.stringify(parameterObj);
+  const parameter = querystring.stringify(parameterObj);
   const urlQuery = url+ '?' + parameter + '&' + qs;
 
   utils.apiCall(urlQuery, function(data) {

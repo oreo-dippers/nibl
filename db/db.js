ad10d547e96e6b6ffbf4d24bcc5f801393c74f98
Lisa Gee <zhusufeng@gmail.com>
2017-09-11 11:57:07 -0700

    Refine documentation in db

--- db/db.js
+++ db/db.js
@@ -18,7 +18,7 @@ db
   });
 
 // Table 1: User (in DB will be: users)
-// This is the table of all our users
+// This is the table of all our users.
 const User = db.define("user", {
   id: {
     type: Sequelize.INTEGER,
@@ -30,9 +30,9 @@ const User = db.define("user", {
 
 // Table 2: Restaurant (in DB will be: restaurants)
 // This is the table of restaurants that have been
-// retrieved from the Foursquare API
+// retrieved from the Foursquare API.
 // To get avgRating, we calculate the avgRatings
-// of all the dishes a restaurant has
+// of all the dishes a restaurant has.
 const Restaurant = db.define("restaurant", {
   id: {
     type: Sequelize.INTEGER,
@@ -50,9 +50,9 @@ const Restaurant = db.define("restaurant", {
 
 // Table 3: Dish (in DB will be: dishes)
 // This is the table of dishes (or menu items) that have been
-// retrieved from the Foursquare API
+// retrieved from the Foursquare API.
 // To get avgRating, we calculate the starRatings
-// from dishReviews
+// from dishReviews.
 const Dish = db.define("dish", {
   id: {
     type: Sequelize.INTEGER,
@@ -68,8 +68,8 @@ const Dish = db.define("dish", {
 });
 
 // Table 4: DishReview (in DB will be: dishReviews)
-// This is the table of reviews for dishes
-// A user can only leave one review per dish
+// This is the table of reviews for dishes.
+// A user can only leave one review per dish.
 const DishReview = db.define("dishReview", {
   id: {
     type: Sequelize.INTEGER,
@@ -94,8 +94,8 @@ const DishReview = db.define("dishReview", {
 });
 
 // Table 5: DishReviewUpvote (in DB will be: dishReviewUpvotes)
-// This is the table of upvotes for reviews of dishes
-// A user can only upvote once per dishReview
+// This is the table of upvotes for reviews of dishes.
+// A user can only upvote once per dishReview.
 const DishReviewUpvote = db.define("dishReviewUpvote", {
   id: {
     type: Sequelize.INTEGER,
@@ -107,24 +107,25 @@ const DishReviewUpvote = db.define("dishReviewUpvote", {
 
 // Table 6: UserDish (in DB will be: userDishes)
 // This is the table of all dishes that a user saves
-// that they are interested in, so that they can order next time
+// that they are interested in, so that they can order next time.
 // This is necessary so that the table will be 'userDishes'
-// instead of 'UserDish' in DB
-// This way we can refer to it like Tables 1-5
+// instead of 'UserDish' in DB.
+// This way we can refer to it like Tables 1-5.
 const UserDish = db.define("userDish", {});
 
 // Table 7: UserRestaurant (in DB will be: userRestaurants)
 // This is the table of all restaurants that a user saves
-// that they are interested in, so that they can visit next time
+// that they are interested in, so that they can visit next time.
 // This is necessary so that the table will be 'userRestaurants'
-// instead of 'UserRestaurant' in DB
-// This way we can refer to it like Tables 1-5
+// instead of 'UserRestaurant' in DB.
+// This way we can refer to it like Tables 1-5.
 const UserRestaurant = db.define("userRestaurant", {});
 
 // Table 8: SearchResult (in DB will be: searchResults)
-// This is the table of search queries that users make
-// In the case that the Foursquare API goes down, we can
-// give some information to the user
+// This is the table of search queries that users make.
+// In the case that the Foursquare API goes down, we may
+// be able to give the user a response if their query
+// already exists in this table.
 const SearchResult = db.define("searchResult", {
   id: {
     type: Sequelize.INTEGER,

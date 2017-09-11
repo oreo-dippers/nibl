a1e9fcdc62979bee934662b83df86fcf4fb6683e
Lisa Gee <zhusufeng@gmail.com>
2017-09-11 11:11:09 -0700

    Add Restaurant Model to db

--- db/db.js
+++ db/db.js
@@ -1,25 +1,24 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 // For local variable loading
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.load();
 
 // Make new Sequelize instance to PG DB linked
 const db = new Sequelize(process.env.POSTGRES_URL);
 
-// Ensure db makes connection 
+// Ensure db makes connection
 db
   .authenticate()
   .then(() => {
-    console.log('Postgres Database connection established successfully!');
+    console.log("Postgres Database connection established successfully!");
   })
   .catch(err => {
-    console.error('Postgres Database connection failed: ', err);
+    console.error("Postgres Database connection failed: ", err);
   });
 
-
 // Table 1: User (in DB will be: users)
-const User = db.define('user', {
+const User = db.define("user", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
@@ -28,12 +27,27 @@ const User = db.define('user', {
   firebaseUuid: Sequelize.STRING
 });
 
+// Table 2: Restaurant (in DB will be: restaurants)
+const Restaurant = db.define("restaurant", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  foursquareId: Sequelize.STRING,
+  name: Sequelize.STRING,
+  phone: Sequelize.STRING,
+  address: Sequelize.STRING,
+  website: Sequelize.STRING,
+  imageUrl: Sequelize.STRING,
+  avgRating: Sequelize.FLOAT
+});
 
 // Sync the database
 db.sync();
 
-
 module.exports = {
   db: db,
-  User: User
+  User: User,
+  Restaurant: Restaurant
 };

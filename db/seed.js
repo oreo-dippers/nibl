294fd02c12d0fab062915117eb6e926c53466969
Lisa Gee <zhusufeng@gmail.com>
2017-09-11 12:26:26 -0700

    Add drop and database resync in seed

--- db/seed.js
+++ db/seed.js
@@ -5,3 +5,12 @@ const restaurantData = require('./seedData/restaurantData.json');
 // 1 Drop the tables
 // 2 Populate with mock data from seedData folder
 
+// Step 1: Drop the tables
+connection.db.drop()
+  .then(function() {
+    // After dropped, show confirmation
+    console.log('!!! Running seed.js: Tables have been dropped');
+
+    // Set up the database tables again
+    connection.db.sync()
+  });

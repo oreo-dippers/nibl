0f807364d146a427c2cee24f34953652cd6fc2c0
Lisa Gee <zhusufeng@gmail.com>
2017-09-11 12:24:33 -0700

    Start seed file

---
+++ db/seed.js
@@ -0,0 +1,7 @@
+const connection = require('./db.js');
+const restaurantData = require('./seedData/restaurantData.json');
+
+// To seed
+// 1 Drop the tables
+// 2 Populate with mock data from seedData folder
+

64b2a063b593619aa89d8b56ba98a1da303e78be
Lisa Gee <zhusufeng@gmail.com>
2017-09-11 11:44:29 -0700

    Add comment documentation on our db

--- db/db.js
+++ db/db.js
@@ -18,6 +18,7 @@ db
   });
 
 // Table 1: User (in DB will be: users)
+// This is the table of all our users
 const User = db.define("user", {
   id: {
     type: Sequelize.INTEGER,
@@ -28,6 +29,10 @@ const User = db.define("user", {
 });
 
 // Table 2: Restaurant (in DB will be: restaurants)
+// This is the table of restaurants that have been 
+// retrieved from the Foursquare API
+// To get avgRating, we calculate the avgRatings
+// of all the dishes a restaurant has
 const Restaurant = db.define("restaurant", {
   id: {
     type: Sequelize.INTEGER,
@@ -44,6 +49,10 @@ const Restaurant = db.define("restaurant", {
 });
 
 // Table 3: Dish (in DB will be: dishes)
+// This is the table of dishes (or menu items) that have been
+// retrieved from the Foursquare API
+// To get avgRating, we calculate the starRatings
+// from dishReviews
 const Dish = db.define('dish', {
   id: {
     type: Sequelize.INTEGER,
@@ -59,6 +68,8 @@ const Dish = db.define('dish', {
 });
 
 // Table 4: DishReview (in DB will be: dishReviews)
+// This is the table of reviews for dishes
+// A user can only leave one review per dish
 const DishReview = db.define('dishReview', {
   id: {
     type: Sequelize.INTEGER,
@@ -82,6 +93,8 @@ const DishReview = db.define('dishReview', {
 });
 
 // Table 5: DishReviewUpvote (in DB will be: dishReviewUpvotes)
+// This is the table of upvotes for reviews of dishes
+// A user can only upvote once per dishReview
 const DishReviewUpvote = db.define('dishReviewUpvote', {
   id: {
     type: Sequelize.INTEGER,
@@ -92,9 +105,18 @@ const DishReviewUpvote = db.define('dishReviewUpvote', {
 });
 
 // Table 6: UserDish (in DB will be: userDishes)
-// This is necessary so that the table won't be 'UserDish' in DB
-// And so that we can refer to it like the others
+// This is the table 
+// This is necessary so that the table will be 'userDishes' 
+// instead of 'UserDish' in DB
+// This way we can refer to it like Tables 1-5
 const UserDish = db.define('userDish', {
+});
+
+// Table 7: UserRestaurant (in DB will be: userRestaurants)
+// This is necessary so that the table will be 'userRestaurants' 
+// instead of 'UserRestaurant' in DB
+// This way we can refer to it like Tables 1-5
+const UserRestaurant = db.define('userRestaurant', {
 });
 
 // Define Table Associations
@@ -121,5 +143,6 @@ module.exports = {
   Dish: Dish,
   DishReview: DishReview,
   DishReviewUpvote: DishReviewUpvote,
-  UserDish: UserDish
+  UserDish: UserDish,
+  UserRestaurant: UserRestaurant
 };

fa761b0d5d54443d721e432f12d4015773a0a25f
Lisa Gee <zhusufeng@gmail.com>
2017-09-11 12:35:12 -0700

    Add ability to seed restData to rest table

--- db/seed.js
+++ db/seed.js
@@ -13,4 +13,31 @@ connection.db.drop()
 
     // Set up the database tables again
     connection.db.sync()
+      .then(function() {
+        // Seed database with mock data
+
+        // Start with restaurant table
+        restaurantData.forEach(function(element) {
+          console.log(element);
+
+          connection.Restaurant.create({
+            foursquareId: element.foursquareId,
+            name: element.name,
+            phone: element.phone,
+            address: element.address,
+            website: element.website,
+            imageUrl: element.imageUrl,
+            avgRating: element.avgRating
+          })
+          .then(function() {
+            console.log('!!! Running seed.js: Your element has been added to the database successfully!');
+          })
+          .catch(function(e) {
+            console.error('!!! Running seed.js: There was an error adding the element to the database: ', e);
+          });
+        });
+      })
+      .catch(function(e) {
+        console.error('!!! Running seed.js: Database did not sync correctly. See error: ', e);
+      });
   });

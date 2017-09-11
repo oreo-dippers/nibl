26c4f430380aaed52cd9502af08b5f9d3bbcaf7d
Ankit09osr <ankit09osr@gmail.com>
2017-09-11 13:16:54 -0700

    Check environmental variable for firebase

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 var app = require('./serverConfig');
-
+console.log('This is the value', functions.config());
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //

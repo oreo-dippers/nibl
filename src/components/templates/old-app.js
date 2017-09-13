eeac170c5923c2357d87c49a4377a4679dbe764d
d-kang <david.kang714@gmail.com>
2017-09-13 08:32:12 -0700

    Discard Old Components And Save for Reference

---
+++ src/components/templates/old-app.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Form, Button, Menu } from 'semantic-ui-react';
+
+import axios from 'axios';
+import Header from './header';
+import Main from './main';
+import Footer from './footer';
+import SearchBar from './searchBar';
+import Home from './home';
+
+const FIREBASE = `http://localhost:5001/oreo-nibl/us-central1/app`;
+const EXPRESS = `http://localhost:3006`;
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			host: 'dev',
+			restaurants: [],
+			restaurantQuery: ''
+		};
+		this.submitrestaurantQuery = this.submitrestaurantQuery.bind(this);
+	}
+
+	componentDidMount() {}
+
+	submitrestaurantQuery(restaurantQuery) {
+		
+		// let restaurantQuery = this._restaurants.value;
+		this.setState({ restaurantQuery });
+		console.log('restaurantQuery', restaurantQuery);
+
+		// example post
+		// const data = {
+		//   query: restaurantQuery,
+		//   near: 'Los Angeles, CA, United States',
+		//   radius: '5000'
+		// }
+		// axios.post(`${DEV}/test`, data)
+		//   .then((res)=> {
+		//     console.log('successful post!')
+		//     console.log('res.data', res.data);
+		//   })
+		//   .catch((err) => {
+		//     console.log(err)
+		//   })
+
+		var request = {
+			params: {
+				query: restaurantQuery,
+				near: 'Los Angeles, CA, United States',
+				radius: '5000'
+			}
+		};
+		axios
+			.get(`${EXPRESS}/api/restaurants`, request)
+			.then(res => {
+				console.log('successful get!');
+				console.log('res', res.data);
+				var restaurants = res.data.response.groups[0].items;
+				console.log('restaurants', restaurants);
+				this.setState({ restaurants });
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
+
+	render() {
+		const { restaurants } = this.state;
+		return (
+			<div>
+				<Header />
+				
+				<SearchBar restaurantQuery={this.state.restaurantQuery} submitrestaurantQuery={this.submitrestaurantQuery}/>
+
+				<Main restaurants={restaurants} />
+				<Footer />
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/templates/old-dishes.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import firebase from './firebase';
+import { storage, database } from './firebase';
+import CommentCard from './CommentCard';
+import { Form } from 'semantic-ui-react'
+
+const ENV_MODE = 'dev'
+const PROD = `https://us-central1-oreo-nibl.cloudfunctions.net/app`
+const DEV = `http://localhost:5001/oreo-nibl/us-central1/app`
+
+const HOST = ENV_MODE === 'dev' ? DEV : PROD;
+
+
+export default class Dishes extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      dishQuery: '',
+    };
+    this.submitDishQuery = this.submitDishQuery.bind(this);
+  }
+
+  submitDishQuery(e) {
+    e.preventDefault();
+    let dishQuery = this._dishes.value;
+    this.setState({dishQuery});
+    console.log('dishQuery', dishQuery)
+    var data = {
+      query: dishQuery
+    }
+
+    axios.post(`${HOST}/test`, data)
+      .then((res)=> {
+        console.log('successful post!')
+        console.log('res.data', res.data);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+
+
+    var request = {
+      params: {
+        query: dishQuery
+      }
+    }
+    axios.get(`${HOST}/test`, request)
+      .then((res)=> {
+        console.log('successful get!');
+        console.log('res', res);
+      })
+      .catch((err) => {
+        console.log( err)
+      })
+  }
+
+  
+
+  render() {
+    return (
+      <div className="ui container">
+
+      <CommentCard /> 
+
+    {/*
+      <h1> Comment History </h1>
+      
+        <li>"Ramen Depot was delicissssous!"</li>
+        <li>"I love tiny tacos!"</li>
+        <li>"100% would come back to this place for their donuts"</li>
+        <li>"If you like fries, you would love these loaded tater tots"</li>
+      <h1> Liked Dishes </h1>
+        <li>Taco from @TacoBell</li>
+        <li>Burrito from @TacoBell</li>
+        <li>Sandwich from @CarlsJn</li>
+        <li>Salad from @McDonalds</li>
+        <li>Chicken from @ChickenHeaven</li>
+        <li>Steak from @SteakFactory</li>
+</div>
+        <Form onSubmit={this.submitDishQuery}>
+          <Form.Field>
+            <label>Find Dishes</label>
+            <input
+              ref={(d)=>this._dishes = d}
+            />
+          </Form.Field>
+        </Form>
+        
+         You Searched for {this.state.dishQuery}
+        */}
+
+       
+      </div>
+    )
+  }
+};

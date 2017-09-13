fca071bad589a3c5d9a517a5d1acf91018e17bc4
Ankit09osr <ankit09osr@gmail.com>
2017-09-12 17:16:01 -0700

    Create getRestaurantData() to save and get data from Restaurant table

--- server/util.js
+++ server/util.js
@@ -1,4 +1,5 @@
-const request = require('request')
+const request = require('request');
+const db = require('../db/db.js');
 
 module.exports.apiCall = function(url, callback) {
     request(url, function (error, response, body) {
@@ -14,4 +15,44 @@ module.exports.apiCall = function(url, callback) {
 
     callback(JSON.parse(body));
   });
+};
+
+module.exports.getRestaurantData = function(data, callback) {
+    // Save data into Restaurant table
+    //data.response.groups.items is the array of restaurants received
+    var restaurantArray = data.response.groups[0].items;
+    var restaurantData = [];
+
+    // Add each restaurant from response to database
+    restaurantArray.forEach(function (element) {
+      db.Restaurant.findOrCreate({
+        where: {
+          foursquareId: element.venue.id
+        },
+        defaults: {
+          // If it is not in the Restaurant table, set these defaults:
+          foursquareId: element.venue.id,
+          name: element.venue.name,
+          phone: element.venue.contact.formattedPhone,
+          address: JSON.stringify(element.venue.location.formattedAddress),
+          website: element.venue.url,
+          imageUrl: JSON.stringify(element.venue.featuredPhotos.items[0]),
+          avgRating: 0
+        }
+      })
+      .then(function(restaurant) {
+        // console.log('************************************************\n', restaurant);
+        // This gets 1 restaurants ratings:
+        // console.log('Here is this restaurants rating: ', restaurant[0].dataValues);
+        let {foursquareId, name, phone, address, imageUrl, avgRating} = restaurant[0].dataValues;
+        // console.log('Here is this restaurants rating: ', restaurant[0].dataValues.avgRating);
+
+        restaurantData.push({foursquareId, name, phone, address, imageUrl, avgRating});
+      });
+    }); // forEach ends
+
+    // if(restaurantData.length > 0){
+    //   console.log('Callback with restaurantData running')
+    //   callback(restaurantData);
+    // }
 };

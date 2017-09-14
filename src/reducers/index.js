ca5c39e0cebc25f0c8ebd46d743d7331b4ec95c1
d-kang <david.kang714@gmail.com>
2017-09-14 05:27:12 -0700

    Add Router Reducer to Reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
-
+import { routerReducer } from 'react-router-redux'
 // import reducers
 import restaurantsSearchReducer from './restaurantsSearchReducer'
 // combine reducers
 
 const reducers = combineReducers({
-  restaurantsState: restaurantsSearchReducer
+  restaurantsState: restaurantsSearchReducer,
+  routing: routerReducer
 })
 
 

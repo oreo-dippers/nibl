2ce92d7ea42263b9a95a9a92d00cf8d0d8b83cbb
Ankit09osr <ankit09osr@gmail.com>
2017-09-13 22:18:05 -0700

    Make index.js work with new server file structure

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
-var app = require('./serverConfig');
+var app = require('./server/serverConfig');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.app = functions.https.onRequest(app);

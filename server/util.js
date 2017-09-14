69496c32612b662eaa7cf19a55dff28576146df1
Lisa Gee <zhusufeng@gmail.com>
2017-09-14 00:27:20 -0700

    Add getDishData func to util

--- server/util.js
+++ server/util.js
@@ -13,9 +13,65 @@ module.exports.apiCall = function(url, callback) {
     });
 };
 
+module.exports.getDishData = function(data) {
+  var promise = new Promise(function(resolve, reject) {
+
+    // Generate restaurantId
+    // 1 Get foursquare API id, ie: 40a55d80f964a52020f31ee3
+    // 2 Query Restaurant database for it
+    // 3 A If id is found, set it
+    var restaurantId = 1;  // This is a hardcoded example that attaches to a seeded restaurant
+    // 3 B If id is not there, add it to the Restaurant table
+    // This may require a call to the FourSquare API)
+
+    var menus = data.response.menu.menus.items;
+    var dishData = [];
+
+    // Add each dish from response to database
+    menus.forEach(function(menu) {
+      menu.entries.items.forEach(function(section) {
+        section.entries.items.forEach(function(dish) {
+
+          db.Dish.findOrCreate({
+            where: {
+              foursquareEntryId: dish.entryId,
+            },
+            defaults: {
+              // If it is not in the Dish table, set these defaults:
+              restaurantId: restaurantId,
+              foursquareEntryId: dish.entryId,
+              name: dish.name,
+              imageUrl: '',
+              description: dish.description,
+              price: dish.price,
+              avgRating: 0
+            }
+          })
+          .then(function(dish) {
+            // Make organized data to send to front end
+            let {foursquareEntryId, name, imageUrl, description, price, avgRating} = dish[0].dataValues;
+            dishData.push({foursquareEntryId, name, imageUrl, description, price, avgRating});
+          });
+        });
+      });
+    });
+
+    setTimeout(function() {
+      if(dishData.length > 0) {
+        console.log('dish data resolves');
+        resolve(dishData);
+      } else {
+        reject('No data');
+      }
+    }, 1000);
+
+  });
+
+  return promise;
+}
 
 module.exports.getRestaurantData = function(data) {
-  var promise = new Promise(function(resolve, reject){
+  var promise = new Promise(function(resolve, reject) {
 
     // Save data into Restaurant table
     //data.response.groups.items is the array of restaurants received
@@ -45,9 +101,9 @@ module.exports.getRestaurantData = function(data) {
       });
     }); // forEach ends
 
-    setTimeout(function(){
-      if(restaurantData.length > 0){
-        console.log('This thing is working')
+    setTimeout(function() {
+      if(restaurantData.length > 0) {
+        console.log('restaurant data resolves');
         resolve(restaurantData);
       } else {
         reject('No data');

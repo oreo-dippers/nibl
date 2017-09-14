e0ba02f92538f4eac9af636679c90b3d921b0fc7
Ankit09osr <ankit09osr@gmail.com>
2017-09-13 23:08:56 -0700

    Move getRestaurants() to restaurants.js folder

---
+++ functions/server/route_handler/restaurants.js
@@ -0,0 +1,35 @@
+const querystring = require('querystring')
+const foursquare = require('../../../api-config/foursquare-config.js')
+const bodyParser = require('body-parser')
+const utils = require('./../utils/util')
+
+var credentials = {
+  'v': '20170904'
+};
+credentials.client_id = foursquare.CLIENT_ID;
+credentials.client_secret = foursquare.CLIENT_SECRET;
+const qs = querystring.stringify(credentials);
+
+
+module.exports.getRestaurants = function(req, res) {
+  const url = 'https://api.foursquare.com/v2/venues/explore';
+  var myQuery = req.query;
+  var parameterObj = {
+    'venuePhotos': '1',
+    'limit': '10'
+  };
+  parameterObj.query = myQuery.query;
+  parameterObj.near = myQuery.near;
+  parameterObj.radius = myQuery.radius;
+  const parameter = querystring.stringify(parameterObj);
+  const urlQuery = url+ '?' + parameter + '&' + qs;
+
+  utils.apiCall(urlQuery, function(data) {
+    utils.getRestaurantData(data)
+      .then(function(restaurantData){
+        console.log('This this finally worked');
+        res.send(restaurantData);
+       });
+  });
+};
+

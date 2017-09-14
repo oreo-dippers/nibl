4c51156033693864a2b0cf2060f7a253c33b7c7c
Ankit09osr <ankit09osr@gmail.com>
2017-09-13 23:10:55 -0700

    Refactor routeHandler.js to work with restaurants.js

--- functions/server/routeHandler.js
+++ functions/server/routeHandler.js
@@ -2,6 +2,7 @@ const querystring = require('querystring')
 const foursquare = require('../../api-config/foursquare-config.js')
 const bodyParser = require('body-parser')
 const utils = require('./utils/util')
+const restaurants = require('./route_handler/restaurants');
 
 var credentials = {
   'v': '20170904'
@@ -27,27 +28,7 @@ module.exports.getMenu = function(req, res) {
   });
 };
 
-module.exports.getRestaurants = function(req, res) {
-  const url = 'https://api.foursquare.com/v2/venues/explore';
-  var myQuery = req.query;
-  var parameterObj = {
-    'venuePhotos': '1',
-    'limit': '10'
-  };
-  parameterObj.query = myQuery.query;
-  parameterObj.near = myQuery.near;
-  parameterObj.radius = myQuery.radius;
-  const parameter = querystring.stringify(parameterObj);
-  const urlQuery = url+ '?' + parameter + '&' + qs;
-
-  utils.apiCall(urlQuery, function(data) {
-    utils.getRestaurantData(data)
-      .then(function(restaurantData){
-        console.log('This this finally worked');
-        res.send(restaurantData);
-       });
-  });
-};
+module.exports.getRestaurants = restaurants.getRestaurants;
 
 
 module.exports.getDummy = function(req, res) {

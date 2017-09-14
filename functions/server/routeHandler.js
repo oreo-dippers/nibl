062f592d873b69aa6cf77831642d171198e1a71c
Ankit09osr <ankit09osr@gmail.com>
2017-09-13 22:19:23 -0700

    Add server code inside server folder by separating concern

---
+++ functions/server/routeHandler.js
@@ -0,0 +1,55 @@
+const querystring = require('querystring')
+const foursquare = require('../../api-config/foursquare-config.js')
+const bodyParser = require('body-parser')
+const utils = require('./utils/util')
+
+var credentials = {
+  'v': '20170904'
+};
+credentials.client_id = foursquare.CLIENT_ID;
+credentials.client_secret = foursquare.CLIENT_SECRET;
+const qs = querystring.stringify(credentials);
+
+module.exports.getVenue = function(req, res) {
+  const url = 'https://api.foursquare.com/v2/venues/4bf9e1445ec320a11c028bd3';
+  const urlQuery = url+ '?' + qs;
+  utils.apiCall(urlQuery, function(data) {
+    res.send(data.response.venue);
+  });
+
+};
+
+module.exports.getMenu = function(req, res) {
+  const url = 'https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3/menu';
+  const urlQuery = url+ '?' + qs;
+  utils.apiCall(urlQuery, function(data) {
+    res.send(data);
+  });
+};
+
+module.exports.getRestaurants = function(req, res) {
+  const url = 'https://api.foursquare.com/v2/venues/explore';
+  var myQuery = req.query;
+  var parameterObj = {
+    'venuePhotos': '1',
+    'limit': '10'
+  };
+  parameterObj.query = myQuery.query;
+  parameterObj.near = myQuery.near;
+  parameterObj.radius = myQuery.radius;
+  const parameter = querystring.stringify(parameterObj);
+  const urlQuery = url+ '?' + parameter + '&' + qs;
+
+  utils.apiCall(urlQuery, function(data) {
+    utils.getRestaurantData(data)
+      .then(function(restaurantData){
+        console.log('This this finally worked');
+        res.send(restaurantData);
+       });
+  });
+};
+
+
+module.exports.getDummy = function(req, res) {
+  console.log('This dummy is working')
+};
---
+++ functions/server/serverConfig.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const routeHandler = require('./routeHandler');
+const path = require('path');
+const cors = require('cors');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('/venue',routeHandler.getVenue);
+app.get('/menu',routeHandler.getMenu);
+
+//REST endpoint for searching restaurants nearby
+app.get('/api/restaurants/', routeHandler.getRestaurants);
+
+//REST endpoint for getting detail of single restaurant
+app.get('/api/restaurants/page', routeHandler.getDummy);
+
+//REST endpoint for posting review of a dish
+app.post('/api/dishes/review', routeHandler.getDummy);
+
+//REST endpoint for getting user profile
+app.get('/api/user', routeHandler.getDummy);
+
+//REST endpoint for saving user profile
+app.post('/api/user', routeHandler.getDummy);
+
+//REST endpoint for getting user saved Dish
+app.get('/api/user/fridge', routeHandler.getDummy);
+
+//REST endpoint for saving dish user want to eat later.
+app.post('/api/user/fridge', routeHandler.getDummy);
+
+//REST endpoint for getting user saved restaurants
+app.get('/api/user/saved-restaurants', routeHandler.getDummy);
+
+//REST endpoint for saving restaurants user want to visit later.
+app.post('/api/user/saved-restaurants', routeHandler.getDummy);
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(process.env.PWD, 'dist', 'index.html'));
+});
+
+
+module.exports = app;

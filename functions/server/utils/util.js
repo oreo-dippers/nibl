2dd80b7136d49b16d7ee8a7d764d6bea960159b5
Lisa Gee <zhusufeng@gmail.com>
2017-09-14 22:58:36 -0700

    Require postDishReviewData to util

--- functions/server/utils/util.js
+++ functions/server/utils/util.js
@@ -1,6 +1,7 @@
 const requestPromise = require('request-promise');
 const restaurant = require('./restaurantData.js');
 const dish = require('./dishData.js');
+const dishReview = require('./dishReviewData.js');
 const httpRequest = require('./foursquareApiCall.js');
 
 module.exports.apiCall = httpRequest.apiCall;
@@ -8,3 +9,5 @@ module.exports.apiCall = httpRequest.apiCall;
 module.exports.getRestaurantData = restaurant.getRestaurantData;
 
 module.exports.getDishData = dish.getDishData;
+
+module.exports.postDishReviewData = dishReview.postDishReviewData;

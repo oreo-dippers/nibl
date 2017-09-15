4b6ce156ffbd2eec30025032a303a2c3c4772cb6
Lisa Gee <zhusufeng@gmail.com>
2017-09-14 23:08:51 -0700

    Fill out more of postDishReviewData function

--- functions/server/utils/dishReviewData.js
+++ functions/server/utils/dishReviewData.js
@@ -3,13 +3,38 @@ const db = require('../../db/db.js');
 module.exports.postDishReviewData = function(data) {
   let promise = new Promise(function(resolve, reject) {
 
-  // if(dishData.length > 0) {
-  //   console.log('Dish data resolves');
-  //   resolve(dishData);
-  // } else {
-  //   reject('No data');
-  // }
+    // Get the userId based on the firebase UUID
+    let currentUserId;
+    // Get the dishId based on the foursquareEntryId
+    let currentDishId;
 
+    db.DishReview.findOrCreate({
+      where: {
+        userId: currentUserId,
+        dishId: currentDishId
+      },
+      defaults: {
+        // If it is not in the DishReview table, set these defaults:
+        userId: currentUserId,
+        dishId: currentDishId,
+        review: data.review,
+        starRating: data.starRating,
+        imageUrl: data.imageUrl,
+        upvoteTotal: 0
+      }
+    })
+    .then(function(dishReview) {
+      // Make organized data to send to front end
+      let {userId, dishId, review, starRating, imageUrl, upvoteTotal} = dishReview[0].dataValues;
+
+      let newReview = {userId, dishId, review, starRating, imageUrl, upvoteTotal};
+      if(newReview) {
+        console.log('Dish Review was added to database!');
+        resolve(newReview);
+      } else {
+        reject('No data');
+      }
+    });
   });
 
   return promise;

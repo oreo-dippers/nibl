29b1ca89bb94f913cc91b6f507470ef8f197b996
d-kang <david.kang714@gmail.com>
2017-09-15 07:57:05 -0700

    Pass getRestaurants Util Function Dispatch Method

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import store from '../store';
 
 import { getRestaurantsSuccess } from '../actions/restaurantSearch-action';
 
@@ -7,12 +6,13 @@ const FIREBASE = `http://localhost:5001/oreo-nibl/us-central1/app`;
 const EXPRESS = `http://localhost:3006`;
 
 
-export function getRestaurants(request) {
+export function getRestaurants(request, dispatch) {
+
   return axios.get(`${FIREBASE}/api/restaurants`, request)
       .then(res => {
         console.log('successful getRestaurants!');
         console.log('res.data', res.data);
-        store.dispatch(getRestaurantsSuccess(res.data));
+        dispatch(getRestaurantsSuccess(res.data));
       })
       .catch(err => {
         console.log(err);

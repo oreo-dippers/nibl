a70ae453b84ce220b18774ec0e403a0f3a086249
Lisa Gee <zhusufeng@gmail.com>
2017-09-14 22:59:15 -0700

    Create skeleton of promise for dishReviewData

---
+++ functions/server/utils/dishReviewData.js
@@ -0,0 +1,16 @@
+const db = require('../../db/db.js');
+
+module.exports.postDishReviewData = function(data) {
+  let promise = new Promise(function(resolve, reject) {
+
+  // if(dishData.length > 0) {
+  //   console.log('Dish data resolves');
+  //   resolve(dishData);
+  // } else {
+  //   reject('No data');
+  // }
+
+  });
+
+  return promise;
+};

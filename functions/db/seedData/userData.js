5145bbe207ac4770205fd363e907e1f08f39db12
Lisa Gee <zhusufeng@gmail.com>
2017-09-16 12:33:13 -0700

    Create seed data for user table

---
+++ functions/db/seedData/userData.js
@@ -0,0 +1,14 @@
+module.exports.restaurantData = [
+  {
+    firebaseUuid: 'ExphzGM382Y6lVZDPi82gPXuhPu1'
+  },
+  {
+    firebaseUuid: 'I8bL1aQKE2cr3jrna6geEpf4bWZ2'
+  },
+  {
+    firebaseUuid: 'drlnGKwWknYALR1rA45apQlF6y13'
+  },
+  {
+    firebaseUuid: 'w2ljLy2P9gNbdh6xF0tVgtTf49u1'
+  }
+]

aebf09c7a2e613461af80752b04e3d89d17ca1cf
Lisa Gee <zhusufeng@gmail.com>
2017-09-16 16:33:13 -0700

    Create urlQuery to dynamically get restaurant menu

--- functions/server/route_handler/restaurants.js
+++ functions/server/route_handler/restaurants.js
@@ -35,15 +35,20 @@ module.exports.getRestaurants = function(req, res) {
 
 // Reached at localhost:3006/menu
 module.exports.getMenu = function(req, res) {
-  console.log('Request received has ', req.body);
-  const url = 'https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3/menu';
+  console.log('FoursquareId received is ', req.query.foursquareId);
+  const url = 'https://api.foursquare.com/v2/venues/';
+  const foursquareId = req.query.foursquareId;
+
+  // const url = 'https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3/menu';
   // This is another url to play with to ensure code below works
   // const url = 'https://api.foursquare.com/v2/venues/47a1bddbf964a5207a4d1fe3/menu';
-  const urlQuery = url+ '?' + qs;
-
+  // const urlQuery = url+ '?' + qs;
+  const urlQuery = url + foursquareId + '/menu' + '?' + qs;
+  console.log('urlQuery is ', urlQuery);
   utils.apiCall(urlQuery, function(data) {
     utils.getDishData(data)
       .then(function(dishData) {
+        console.log(dishData);
         console.log('dishData sent to front end');
         res.send(dishData);
       });

fb0c4e39db27f49adf89ce17bb79ebf06a4523ba
Lisa Gee <zhusufeng@gmail.com>
2017-09-15 18:21:48 -0700

    Update the inner folder with updated seed Data

--- functions/db/seedData/restaurantData.js
+++ functions/db/seedData/restaurantData.js
@@ -1,10 +1,11 @@
-[
+module.exports.restaurantData = [
   {
     foursquareId: "50c92ed6e4b08539f9ff721c",
     name: "Bar Amá",
     phone: "(213) 687-8002",
     address:
       "['118 W 4th St (at Main St)','Los Angeles, CA 90013','United States']",
+    website: 'http://www.bar-ama.com',
     imageUrl:
       "{'id':'526ae19311d26a22469c8a2f','createdAt':1382736275,'prefix':'https://igx.4sqi.net/img/general/','suffix':'/47264_mBrX8u-BvHuuQoUE6BsRQ-TjP-Yc1FygGaKw9c6mOM8.jpg','width':640,'height':640,'user':{'id':'47264','firstName':'Matt','lastName':'Duckor','gender':'male','photo':{'prefix':'https://igx.4sqi.net/img/user/','suffix':'/47264_1253592654323.jpg'}},'visibility':'public'}"
   },
@@ -14,6 +15,7 @@
     phone: "(213) 627-7656",
     address:
       "['541 S Spring St (at 6th St)','Los Angeles, CA 90013','United States']",
+    website: 'http://guisados.co',
     imageUrl:
       "{'id':'5544780e498e5eb1fe62a912','createdAt':1430550542,'prefix':'https://igx.4sqi.net/img/general/','suffix':'/4752182_b1tmyiYBHCWQhJa6opFzs8GCthE-G6AiDoUbz18ofKg.jpg','width':640,'height':640,'user':{'id':'4752182','firstName':'del','gender':'none','photo':{'prefix':'https://igx.4sqi.net/img/user/','suffix':'/BCIKPYQRSPKLNC1B.jpg'}},'visibility':'public'}"
   },
@@ -22,6 +24,7 @@
     name: "Guerrilla Tacos",
     phone: "(818) 640-3033",
     address: "['826 E 3rd St','Los Angeles, CA 90013','United States']",
+    website: 'http://www.guerrillatacos.com/',
     imageUrl:
       "{'id':'51c4a5b7498ea3991a501f88','createdAt':1371841975,'prefix':'https://igx.4sqi.net/img/general/','suffix':'/38975224_CERu9QZiDcXhcGOjq4W_BvWcP0UB28AvkZVECSlWp4A.jpg','width':612,'height':612,'user':{'id':'38975224','firstName':'Sherry','lastName':'Ali','gender':'female','photo':{'prefix':'https://igx.4sqi.net/img/user/','suffix':'/38975224-QJ0EA4DFWFTYQTCI.jpg'}},'visibility':'public'}"
   },
@@ -31,6 +34,7 @@
     phone: "(213) 620-1071",
     address:
       "['317 S Broadway (Grand Central Market, Space A-5)','Los Angeles, CA 90013','United States']",
+    website: 'http://www.grandcentralsquare.com/',
     imageUrl:
       "{'id':'52f7c78b498e7c10f9859be2','createdAt':1391970187,'prefix':'https://igx.4sqi.net/img/general/','suffix':'/60270_DO0RjeY6adyK4l367F6rx-s5Dt1f1tk21t5usLOALHM.jpg','width':640,'height':640,'user':{'id':'60270','firstName':'Remil','lastName':'Mangali','gender':'male','photo':{'prefix':'https://igx.4sqi.net/img/user/','suffix':'/1NPZ3N4URV32GKAI.jpg'}},'visibility':'public'}"
   },
@@ -40,6 +44,7 @@
     phone: "(213) 613-0416",
     address:
       "['702 N Figueroa St (at Bartlett St)','Los Angeles, CA 90012','United States']",
+    website: 'http://mexicalitaco.com/',
     imageUrl:
       "{'id':'519d3106498eee4c7d2ff68b','createdAt':1369256198,'prefix':'https://igx.4sqi.net/img/general/','suffix':'/148291_SfZXxHiejH0Gp7bYyJ9v8idjOkBg9nDcX64nXlU9-bc.jpg','width':612,'height':612,'user':{'id':'148291','firstName':'Midtown Lunch LA','gender':'male','photo':{'prefix':'https://igx.4sqi.net/img/user/','suffix':'/BP5CTKZUTYF23S30.jpg'}},'visibility':'public'}"
   },
@@ -48,6 +53,7 @@
     name: "Guisados",
     phone: "(213) 250-7600",
     address: "['1261 W Sunset Blvd','Los Angeles, CA 90026','United States']",
+    website: 'http://guisados.co/',
     imageUrl:
       "{'id':'50e4af62e4b0dfa8e0b251ee','createdAt':1357164386,'prefix':'https://igx.4sqi.net/img/general/','suffix':'/41362714_my7_M2aSHAlmblVr4NMMVwVUJ1cnQLfp3A9rwNdUBlI.jpg','width':431,'height':720,'user':{'id':'41362714','firstName':'Zuma','lastName':'Arechiga','gender':'male','photo':{'prefix':'https://igx.4sqi.net/img/user/','suffix':'/2KKODU54FVKI0J3Z.jpg'}},'visibility':'public'}"
   },
@@ -56,6 +62,7 @@
     name: "Villa Moreliana",
     phone: null,
     address: "['Grand Central Market','Los Angeles, CA','United States']",
+    website: null,
     imageUrl:
       "{'id':'597548519fca56562131d47f','createdAt':1500858449,'prefix':'https://igx.4sqi.net/img/general/','suffix':'/89096954_2Sv3vvrFk7E6uVGfxpikynrTuOMVLLdZzwALxUyqtco.jpg','width':1900,'height':1425,'user':{'id':'89096954','firstName':'Steep','lastName':'Beach','gender':'none','photo':{'prefix':'https://igx.4sqi.net/img/user/','suffix':'/89096954-R115THAKUEDUT3CW.jpg'}},'visibility':'public'}"
   },
@@ -65,6 +72,7 @@
     phone: "(213) 622-3744",
     address:
       "['514 W 7th St (7th and Grand)','Los Angeles, CA 90014','United States']",
+    website: 'http://bstaqueria.com/',
     imageUrl:
       "{'id':'55314d89498ef6a53dfe1a0b','createdAt':1429294473,'prefix':'https://igx.4sqi.net/img/general/','suffix':'/8676045_TSuS34K5Z0-X4r8fvR4ml38Fo1xJzbCShqFaY_LY5xg.jpg','width':614,'height':409,'user':{'id':'8676045','firstName':'LAist','gender':'none','photo':{'prefix':'https://igx.4sqi.net/img/user/','suffix':'/CNUQ55DM2RJGPTYU.png'},'type':'page'},'visibility':'public'}"
   },
@@ -74,6 +82,7 @@
     phone: "(213) 290-5184",
     address:
       "['208 E 8th St (Los Angeles St)','Los Angeles, CA 90014','United States']",
+    website: 'http://sonoratownla.com/',
     imageUrl:
       "{'id':'58e802770a3d540f09d5535f','createdAt':1491599991,'prefix':'https://igx.4sqi.net/img/general/','suffix':'/61118854_cvJJbDVs0DTtOSa6cNXZK8w08tit0ITS3icBleib_QQ.jpg','width':1440,'height':1920,'user':{'id':'61118854','firstName':'Audrey','lastName':'Ahn','gender':'female','photo':{'prefix':'https://igx.4sqi.net/img/user/','suffix':'/61118854-3V14LR430S0M3Y2A.jpg'}},'visibility':'public'}"
   },
@@ -82,6 +91,7 @@
     name: "La Noche Buena",
     phone: "(213) 628-2078",
     address: "['E-8 Olvera St','Los Angeles, CA 90012','United States']",
+    website: null,
     imageUrl:
       "{'id':'52c766f8498e827a106b8427','createdAt':1388799736,'prefix':'https://igx.4sqi.net/img/general/','suffix':'/47430851_xpXvYsR4HiNjnKJJfYpT14wg27qJqcc6kDGhckSSg9c.jpg','width':960,'height':720,'user':{'id':'47430851','firstName':'Yolanda','lastName':'Medina','gender':'female','photo':{'prefix':'https://igx.4sqi.net/img/user/','suffix':'/CCSWMP1XO1Q30YBC.jpg'}},'visibility':'public'}"
   }

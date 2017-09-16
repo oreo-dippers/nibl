3f67d9800d8a5fd8154a6d91172f9b8d5ca8fcdf
Lisa Gee <zhusufeng@gmail.com>
2017-09-15 18:24:57 -0700

    Reformatted outside db/db.js

--- db/db.js
+++ db/db.js
@@ -12,23 +12,23 @@ const db = new Sequelize(process.env.POSTGRES_URL, {
 
 // Ensure db makes connection
 db
-.authenticate()
-.then(() => {
-  console.log('Postgres Database connection established successfully!');
-})
-.catch(err => {
-  console.error('Postgres Database connection failed: ', err);
-});
+  .authenticate()
+  .then(() => {
+    console.log('Postgres Database connection established successfully!');
+  })
+  .catch(err => {
+    console.error('Postgres Database connection failed: ', err);
+  });
 
 // Table 1: User (in DB will be: users)
 // This is the table of all our users.
 const User = db.define('user', {
-id: {
-  type: Sequelize.INTEGER,
-  autoIncrement: true,
-  primaryKey: true
-},
-firebaseUuid: Sequelize.STRING
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  firebaseUuid: Sequelize.STRING
 });
 
 // Table 2: Restaurant (in DB will be: restaurants)
@@ -37,18 +37,18 @@ firebaseUuid: Sequelize.STRING
 // To get avgRating, we calculate the avgRatings
 // of all the dishes a restaurant has.
 const Restaurant = db.define('restaurant', {
-id: {
-  type: Sequelize.INTEGER,
-  autoIncrement: true,
-  primaryKey: true
-},
-foursquareId: Sequelize.STRING,
-name: Sequelize.STRING,
-phone: Sequelize.STRING,
-address: Sequelize.JSON,
-website: Sequelize.STRING,
-imageUrl: Sequelize.JSON,
-avgRating: Sequelize.FLOAT
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  foursquareId: Sequelize.STRING,
+  name: Sequelize.STRING,
+  phone: Sequelize.STRING,
+  address: Sequelize.JSON,
+  website: Sequelize.STRING,
+  imageUrl: Sequelize.JSON,
+  avgRating: Sequelize.FLOAT
 });
 
 // Table 3: Dish (in DB will be: dishes)
@@ -57,55 +57,55 @@ avgRating: Sequelize.FLOAT
 // To get avgRating, we calculate the starRatings
 // from dishReviews.
 const Dish = db.define('dish', {
-id: {
-  type: Sequelize.INTEGER,
-  autoIncrement: true,
-  primaryKey: true
-},
-foursquareEntryId: Sequelize.STRING,
-name: Sequelize.STRING,
-imageUrl: Sequelize.STRING,
-description: Sequelize.TEXT,
-price: Sequelize.STRING,
-avgRating: Sequelize.FLOAT
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  foursquareEntryId: Sequelize.STRING,
+  name: Sequelize.STRING,
+  imageUrl: Sequelize.STRING,
+  description: Sequelize.TEXT,
+  price: Sequelize.STRING,
+  avgRating: Sequelize.FLOAT
 });
 
 // Table 4: DishReview (in DB will be: dishReviews)
 // This is the table of reviews for dishes.
 // A user can only leave one review per dish.
 const DishReview = db.define('dishReview', {
-id: {
-  type: Sequelize.INTEGER,
-  autoIncrement: true,
-  primaryKey: true
-},
-review: {
-  type: Sequelize.STRING,
-  allowNull: false,
-  validate: {
-    // String length must be between 1 to 140
-    len: {
-      args: [1, 140],
-      msg:
-        'The review must be at least 1 character and up to 140 characters long'
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  review: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      // String length must be between 1 to 140
+      len: {
+        args: [1, 140],
+        msg:
+          'The review must be at least 1 character and up to 140 characters long'
+      }
     }
-  }
-},
-starRating: Sequelize.INTEGER,
-imageUrl: Sequelize.JSON,
-upvoteTotal: Sequelize.INTEGER
+  },
+  starRating: Sequelize.INTEGER,
+  imageUrl: Sequelize.JSON,
+  upvoteTotal: Sequelize.INTEGER
 });
 
 // Table 5: DishReviewUpvote (in DB will be: dishReviewUpvotes)
 // This is the table of upvotes for reviews of dishes.
 // A user can only upvote once per dishReview.
 const DishReviewUpvote = db.define('dishReviewUpvote', {
-id: {
-  type: Sequelize.INTEGER,
-  autoIncrement: true,
-  primaryKey: true
-},
-upvote: Sequelize.BOOLEAN
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  upvote: Sequelize.BOOLEAN
 });
 
 // Table 6: UserDish (in DB will be: userDishes)
@@ -130,13 +130,13 @@ const UserRestaurant = db.define('userRestaurant', {});
 // be able to give the user a response if their query
 // already exists in this table.
 const SearchResult = db.define('searchResult', {
-id: {
-  type: Sequelize.INTEGER,
-  autoIncrement: true,
-  primaryKey: true
-},
-queryString: Sequelize.STRING,
-result: Sequelize.JSON
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  queryString: Sequelize.STRING,
+  result: Sequelize.JSON
 });
 
 // Define Table Associations
@@ -161,19 +161,19 @@ User.hasMany(SearchResult);
 
 // Sync the database
 db.sync(
-{
-  // logging: console.log
-}
+  {
+    // logging: console.log
+  }
 );
 
 module.exports = {
-db: db,
-User: User,
-Restaurant: Restaurant,
-Dish: Dish,
-DishReview: DishReview,
-DishReviewUpvote: DishReviewUpvote,
-UserDish: UserDish,
-UserRestaurant: UserRestaurant,
-SearchResult: SearchResult
+  db: db,
+  User: User,
+  Restaurant: Restaurant,
+  Dish: Dish,
+  DishReview: DishReview,
+  DishReviewUpvote: DishReviewUpvote,
+  UserDish: UserDish,
+  UserRestaurant: UserRestaurant,
+  SearchResult: SearchResult
 };

4366b675a5a8f1b3eca2cff6d1d8565d3d79c6bc
d-kang <david.kang714@gmail.com>
2017-09-17 08:21:27 -0700

    Add Restaurants MenuCard

---
+++ src/components/views/restaurants/menuCard.jsx
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Button,
+  Header,
+  Image,
+  Modal,
+  Comment,
+  Feed,
+  Popup,
+  Icon,
+  Card,
+  Rating,
+} from 'semantic-ui-react';
+
+class MenuCard extends Component {
+  render() {
+    const {menu} = this.props;
+    const dish = menu
+    console.log(menu);
+    return (
+      <div>
+        <Card>
+          <Image
+            src="https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12751127_511734799019156_1460279120_n.jpg"
+            alt=""
+          />
+
+          <Card.Content>
+            <Card.Header>
+              <Link to={
+                  {
+                    pathname: `/dishes/${menu.name}`,
+                    state: { dish }
+                  }
+                }>
+                {menu.name}
+              </Link>
+            </Card.Header>
+
+            <Rating defaultRating={3} maxRating={5} disabled />
+            <Popup
+              trigger={<Card.Description>{menu.description ? menu.description.slice(0, 30)+'...' : 'No description'} </Card.Description>}
+              content={menu.description}
+            />
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="dollar" />
+              {menu.price}
+            </a>
+          </Card.Content>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default MenuCard;

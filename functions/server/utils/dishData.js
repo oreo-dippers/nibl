61118723169743598fe41a2433262be86587f8a3
Lisa Gee <zhusufeng@gmail.com>
2017-09-17 00:15:00 -0700

    Clean up code and comments in dishData.js

--- functions/server/utils/dishData.js
+++ functions/server/utils/dishData.js
@@ -1,40 +1,35 @@
 const db = require('../../db/db.js');
 
 module.exports.getDishData = function(data, foursquareId) {
-  console.log('!!!Received foursquareId: ', foursquareId);
   
-  // var restaurantId;
-  var dishData = [];
-
   const promise = new Promise((resolve, reject) => {
-
-    // Generate restaurantId
-    // 1 Get foursquare id, ie: 40a55d80f964a52020f31ee3
-    // 2 Query Restaurant database for it
+    // Query Restaurant database for restaurantId
     db.Restaurant.findOne({
       where: {
         foursquareId: foursquareId
       }
     })
     .then((restaurant) => {
-      console.log('Restaurant id was found: ', restaurant.dataValues.id);
-
-      // 3 A If id is found, set it
+      // 1 If id is found, set it
       const restaurantId = restaurant.dataValues.id;
 
-      // 3 B If id is not there, add it to the Restaurant table
+      // 2 If id is not there, add it to the Restaurant table
       // This may require a call to the FourSquare API)
+      // TODO
 
       return restaurantId;
     })
     .then((restaurantId) => {
+      // Not all restaurants have menus
       console.log('Menu count is ', data.response.menu.menus.count);
-      // Set where to start iterating over menus
-      if (data.response.menu.menus.count > 0){
 
+      // If there is a menu
+      if (data.response.menu.menus.count > 0){
+        // Set where to start iterating over menus
         var menus = data.response.menu.menus.items;
         var dishesToAddArray = [];
 
+        // Get all dishes from menu
         menus.forEach((menu) => {
           menu.entries.items.forEach((section) => {
             section.entries.items.forEach((dish) => {
@@ -43,7 +38,8 @@ module.exports.getDishData = function(data, foursquareId) {
           });
         });
       
-        var fn = function(dish) {
+        // Function to findOrCreate dishes in database
+        var addDishToDB = function(dish) {
           return new Promise((resolve) => {
             db.Dish.findOrCreate({
               where: {
@@ -62,83 +58,33 @@ module.exports.getDishData = function(data, foursquareId) {
             })
             .then((currentDish) => {
               // Make organized data to send to front end
-              var {foursquareEntryId, name, imageUrl, description, price, avgRating} = currentDish[0].dataValues;
+              let {foursquareEntryId, name, imageUrl, description, price, avgRating} = currentDish[0].dataValues;
 
-              console.log('Dish Item added to db is ', {foursquareEntryId, name, imageUrl, description, price, avgRating});
-
-              // return currentDish;
               resolve({foursquareEntryId, name, imageUrl, description, price, avgRating});
-              // dishData.push({foursquareEntryId, name, imageUrl, description, price, avgRating});
             });
           });
         };
 
-        var actions = dishesToAddArray.map(fn);
+        // actions is to iterate through all dishes and addDishToDB
+        var actions = dishesToAddArray.map(addDishToDB);
 
+        // Promise all the actions
         var results = Promise.all(actions);
         
+        // Use the results of Promise.all
         results
-          .then(dishArray => {
-            console.log('!!! Returned correctly. dishArray is ', dishArray);
-            return dishArray;
-            })
           .then((dishArray) => {
-            console.log('!!! dishData is: ', dishArray);
+            console.log('!!! dishArray is: ', dishArray);
             if(dishArray) {
               console.log('Dish data resolves');
               resolve(dishArray);
             } 
-            else {
-              console.log('Dish data rejects/no data');
-              reject(dishArray);
-            }
           });
-
-        // // Add each dish from response to database
-        // menus.forEach((menu) => {
-        //   menu.entries.items.forEach((section) => {
-        //     section.entries.items.forEach((dish) => {
-        //       console.log('dish is ', dish);
-
-        //       db.Dish.findOrCreate({
-        //         where: {
-        //           foursquareEntryId: dish.entryId,
-        //         },
-        //         defaults: {
-        //           // If it is not in the Dish table, set these defaults:
-        //           restaurantId: restaurantId,
-        //           foursquareEntryId: dish.entryId,
-        //           name: dish.name,
-        //           imageUrl: '',
-        //           description: dish.description,
-        //           price: dish.price,
-        //           avgRating: 0
-        //         }
-        //       })
-        //       .then((currentDish) => {
-        //         // Make organized data to send to front end
-        //         var {foursquareEntryId, name, imageUrl, description, price, avgRating} = currentDish[0].dataValues;
-
-        //         console.log({foursquareEntryId, name, imageUrl, description, price, avgRating});
-
-        //         dishData.push({foursquareEntryId, name, imageUrl, description, price, avgRating});
-        //       });
-        //     });
-        //   });
-        // }); // End outermost forEach
       } else {
         console.log('Foursquare does not supply menu here');
+        reject('This restaurant does not have a menu');
       }
     });
-
-    // setTimeout(function() {
-    //   if(dishData.length > 0) {
-    //     console.log('Dish data resolves');
-    //     resolve(dishData);
-    //   } else {
-    //     reject('No data');
-    //   }
-    // }, 1000);
 
   }); // End of promise
 

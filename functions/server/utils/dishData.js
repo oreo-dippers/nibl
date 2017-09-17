36fdf71e763168bde28baeddd6c9bf7429fa1b8b
Lisa Gee <zhusufeng@gmail.com>
2017-09-16 23:01:41 -0700

    Main dishData structure up but to be reformatted

--- functions/server/utils/dishData.js
+++ functions/server/utils/dishData.js
@@ -6,13 +6,6 @@ module.exports.getDishData = function(data, foursquareId) {
   // var restaurantId;
   var dishData = [];
 
-  // Set where to start iterating over menus
-  if (data.response.menu.menus.count > 0){
-    var menus = data.response.menu.menus.items;
-  } else {
-    reject('Foursquare does not supply menu here...');
-  }
-
   const promise = new Promise((resolve, reject) => {
 
     // Generate restaurantId
@@ -32,46 +25,59 @@ module.exports.getDishData = function(data, foursquareId) {
       // 3 B If id is not there, add it to the Restaurant table
       // This may require a call to the FourSquare API)
 
-      return restaurantId
+      return restaurantId;
     })
     .then((restaurantId) => {
-      // Add each dish from response to database
-      menus.forEach((menu) => {
-        menu.entries.items.forEach((section) => {
-          section.entries.items.forEach((dish) => {
-            console.log('dish is ', dish);
+      console.log('Menu count is ', data.response.menu.menus.count);
+      // Set where to start iterating over menus
+      if (data.response.menu.menus.count > 0){
+        var menus = data.response.menu.menus.items;
+      
+        // Add each dish from response to database
+        menus.forEach((menu) => {
+          menu.entries.items.forEach((section) => {
+            section.entries.items.forEach((dish) => {
+              console.log('dish is ', dish);
 
-            db.Dish.findOrCreate({
-              where: {
-                foursquareEntryId: dish.entryId,
-              },
-              defaults: {
-                // If it is not in the Dish table, set these defaults:
-                restaurantId: restaurantId,
-                foursquareEntryId: dish.entryId,
-                name: dish.name,
-                imageUrl: '',
-                description: dish.description,
-                price: dish.price,
-                avgRating: 0
-              }
-            })
-            .then((dish) => {
-              // Make organized data to send to front end
-              let {foursquareEntryId, name, imageUrl, description, price, avgRating} = dish[0].dataValues;
-              dishData.push({foursquareEntryId, name, imageUrl, description, price, avgRating});
+              db.Dish.findOrCreate({
+                where: {
+                  foursquareEntryId: dish.entryId,
+                },
+                defaults: {
+                  // If it is not in the Dish table, set these defaults:
+                  restaurantId: restaurantId,
+                  foursquareEntryId: dish.entryId,
+                  name: dish.name,
+                  imageUrl: '',
+                  description: dish.description,
+                  price: dish.price,
+                  avgRating: 0
+                }
+              })
+              .then((currentDish) => {
+                // Make organized data to send to front end
+                var {foursquareEntryId, name, imageUrl, description, price, avgRating} = currentDish[0].dataValues;
+
+                console.log({foursquareEntryId, name, imageUrl, description, price, avgRating});
+
+                dishData.push({foursquareEntryId, name, imageUrl, description, price, avgRating});
+              });
             });
           });
-        });
-      }); // End outermost forEach
+        }); // End outermost forEach
+      } else {
+        console.log('Foursquare does not supply menu here');
+      }
     }).then(() => {
       console.log('!!! dishData is: ', dishData);
-      if(dishData.length > 0) {
+      if(dishData.length === dishCount) {
         console.log('Dish data resolves');
         resolve(dishData);
-      } else {
-        reject('No data');
-      }
+      } 
+      // else {
+      //   console.log('Dish data rejects/no data');
+      //   reject(dishData);
+      // }
     });
 
     // setTimeout(function() {

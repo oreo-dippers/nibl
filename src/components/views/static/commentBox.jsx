8c1ec5f734651fafa01be0a71521b4a6fa971173
d-kang <david.kang714@gmail.com>
2017-09-17 10:03:06 -0700

    Create Comment Box Component

--- src/components/views/static/commentBox.jsx
+++ src/components/views/static/commentBox.jsx
@@ -1,61 +1,81 @@
 import React, { Component } from 'react';
-// import FileInput from 'react-file-input';
-import firebase, { storage, database } from '../../firebase';
-// import $ from 'jquery';
-// var Rating = require('react-rating');
+import {
+  Button,
+  Header,
+  Image,
+  Modal,
+  Comment,
+  Feed,
+  Segment,
+  Form,
+  Icon,
+  Grid,
+  Rating,
+} from 'semantic-ui-react';
 
 class CommentBox extends Component {
   constructor() {
     super();
     this.state = {
-      editing: null
     }
-    this.edit = this.edit.bind(this);
-    this.remove = this.remove.bind(this);
-    this.save = this.save.bind(this);
-    this.renderNormal = this.renderNormal.bind(this);
-    this.renderForm = this.renderForm.bind(this);
   }
-	getInitialState() {
-    console.log('getting initial state')
-		return this.setState({editing: false});
-	}
-	edit(){
-		this.setState({editing:true});
-	}
-	remove(){
-		console.log('removed comment');
-	}
-	save(){
-		var val = this.refs.newText.value;
-		console.log('new comment: ' + val)
-		this.setState({editing:false});
-	}
-	renderNormal(){
-		return(
-			<div>
-				<div> {this.props.children}</div>
-				<button onClick={this.edit}>Edit</button>
-				<button onClick={this.remove} > Remove </button>
-			</div>
-		)
-	}
-	renderForm(){
-		return(
-			<div>
-				<textarea ref="newText" defaultValue={this.props.children} placeholder="Yum!" />
-				<button onClick={this.save} >Save</button>
-			</div>
-		)
-	}
-	render() {
-		return (
-			<div className="ui container">
-			  <div> {this.props.children} </div>
-				<button onClick={this.edit}>Edit</button>
-				<button onClick={this.remove}> Remove </button>
-			</div>
-		);
-	}
+
+  render() {
+    const { data } = this.props
+    return (
+      <Grid>
+        <Grid.Column width={2} />
+        <Grid.Column width={10}>
+          <Comment.Group>
+            <Comment>
+              <Comment.Avatar src={data.userImage} />
+              <Comment.Content>
+                <Comment.Author as="a">{data.username}</Comment.Author>
+                <Comment.Metadata>
+                  <div>5 days ago</div>
+                </Comment.Metadata>
+                <Comment.Text>{data.review}</Comment.Text>
+                <Feed.Extra images>
+                  <Modal
+                    basic
+                    size="mini"
+                    trigger={
+                      <a>
+                        <img src={data.imageUrl} />
+                      </a>
+                    }
+                  >
+                    <center>
+                      <Image
+                        wrapped
+                        size="medium"
+                        src={data.imageUrl}
+                      />
+                    </center>
+                  </Modal>
+                </Feed.Extra>
+                <Comment.Actions>
+                  <Feed.Meta>
+                    <Feed.Like>
+                      <Icon name="like" />
+                      1 Like
+                    </Feed.Like>
+                  </Feed.Meta>
+                </Comment.Actions>
+              </Comment.Content>
+            </Comment>
+          </Comment.Group>
+        </Grid.Column>
+        <Grid.Column width={3}>
+          <Button icon>
+            <Icon name="chevron up" />
+          </Button>
+          <div> 23k </div>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+
+
 }
 export default CommentBox;

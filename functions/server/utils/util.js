00a4b0ccd1197e114c1af9903fc18ec831920204
Lisa Gee <zhusufeng@gmail.com>
2017-09-17 17:36:19 -0700

    Update util to take inner postUserData function

--- functions/server/utils/util.js
+++ functions/server/utils/util.js
@@ -3,14 +3,18 @@ const restaurant = require('./restaurantData.js');
 const dish = require('./dishData.js');
 const dishReview = require('./dishReviewData.js');
 const getDishReview = require('./getDishReviewData.js');
+const postUser = require('./postUserData.js');
 const httpRequest = require('./foursquareApiCall.js');
 
 module.exports.apiCall = httpRequest.apiCall;
 
+// Restaurant
 module.exports.getRestaurantData = restaurant.getRestaurantData;
-
 module.exports.getDishData = dish.getDishData;
 
+// Dish Review
 module.exports.postDishReviewData = dishReview.postDishReviewData;
-
 module.exports.getDishReviewData = getDishReview.getDishReviewData;
+
+// User
+module.exports.postUserData =  postUser.postUserData;

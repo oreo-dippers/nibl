cd03508aa29a58d9446bd971e1b9a91b150b60fa
Lisa Gee <zhusufeng@gmail.com>
2017-09-17 17:35:57 -0700

    Create user file for user functions

---
+++ functions/server/route_handler/user.js
@@ -0,0 +1,19 @@
+const querystring = require('querystring');
+const foursquare = require('./../api-config/foursquare-config.js');
+const bodyParser = require('body-parser');
+const utils = require('./../utils/util');
+
+const credentials = {
+  'v': '20170904'
+};
+credentials.client_id = foursquare.CLIENT_ID;
+credentials.client_secret = foursquare.CLIENT_SECRET;
+const qs = querystring.stringify(credentials);
+
+module.exports.postUser = (req, res) => {
+  utils.postUserData(req)
+  .then((data) => {
+    console.log('user Data posted successfully!');
+    res.status(201).send(data);
+   });
+};

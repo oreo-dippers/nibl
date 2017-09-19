ab189c5c56f4bfcd450c157c88f074abf5840c00
Kevin Su <kevinsu1212@gmail.com>
2017-09-19 15:56:12 -0700

    cards on home page

--- src/components/views/static/home.jsx
+++ src/components/views/static/home.jsx
@@ -15,18 +15,27 @@ import {
   } from 'semantic-ui-react';
 const Home = () => (
 	<div >
+	
 
 
 	<Image fluid src="https://firebasestorage.googleapis.com/v0/b/oreo-nibl.appspot.com/o/new_folder%2Ftoppart%20copy.png?alt=media&token=7e8f6b38-da28-423c-b75c-5b9bfa4faa84"/>
 
 		
+	<br /><br />
+	<br /><br />
 	<br />
 	<br />
-	<center>Featured Dishes</center>
+	<center><h1>Featured Dishes</h1></center>
+	<br /><br />
 	<br />
 	<br />
 
-		
+	<Card.Group centered itemsPerRow={4}>
+    	<Card color='teal' image="https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12783394_952125858198193_878253493_n.jpg"/>
+    	<Card color='teal' image="https://i.pinimg.com/736x/c5/9b/35/c59b352ccce61abaa9229678d2d4c1b7--lobster-trap-lobster-shack.jpg" />
+   		 <Card color='teal' image="https://i.pinimg.com/originals/c5/5a/47/c55a473b79760dc707ddf0b8d4bfde4e.jpg" />
+   		 <Card color='teal' image="https://i.pinimg.com/originals/b3/5d/9d/b35d9d9ba3bb3f838ba42dcb100b66af.jpg" />
+	</Card.Group>
 		
 		<br />
 		<br />

0f22181e74ca74c9aff1e22cec9f6d489c3df2d9
Lisa Gee <zhusufeng@gmail.com>
2017-09-19 15:39:52 -0700

    Tidy up reject to match other code

--- functions/server/utils/postUserData.js
+++ functions/server/utils/postUserData.js
@@ -29,7 +29,7 @@ module.exports.postUserData = data => {
         if (userData) {
           resolve(userData);
         } else {
-          reject('Post User Data Promise rejected', userData);
+          reject(userData);
         }
       });
 

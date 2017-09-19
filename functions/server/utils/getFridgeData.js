03dffa2feec89bc773d2c0d453e9e924e0d5df8c
Lisa Gee <zhusufeng@gmail.com>
2017-09-19 15:29:23 -0700

    Debug getFridgeData some more

--- functions/server/utils/getFridgeData.js
+++ functions/server/utils/getFridgeData.js
@@ -10,26 +10,36 @@ module.exports.getFridgeData = (data) => {
     db.User
       .findOne({
         where: {
-          firebaseUuid: data.query.firebaseId
+          // firebaseUuid: data.query.firebaseId
+          // firebaseUuid: 'w2ljLy2P9gNbdh6xF0tVgtTf49u1' // has dishes saved
+          firebaseUuid: 'I8bL1aQKE2cr3jrna6geEpf4bWZ2' // no dishes saved
+          // firebaseUuid: '000000000' // fake user
         }
       })
       .then(user => {
-        console.log('user is ', user);
-        console.log('user[0].dataValues is ', user[0].dataValues);
-        console.log('user[0].dataValues id is ', user[0].dataValues.id);
+
+        // In case no user is found
+        if (!user) {
+          reject('User was not found in database. No saved user dishes');
+        }
+
+        // console.log('user is ', user);
+        // console.log('user.dataValues is ', user.dataValues);
+        console.log('user.dataValues id is ', user.dataValues.id);
 
         // Find all user's saved dishes
         db.UserDish
           .findAll({
             where: {
-              userId: user[0].dataValues.id
+              userId: user.dataValues.id
             }
           })
           .then(userSavedDishes => {
             // userSavedDishes is an array of userIds, dishIds
-            console.log('UserSavedDishes are.. ', userSavedDishes);
+            console.log('UserSavedDishes are ', userSavedDishes);
 
-            if (!userSavedDishes) {
+            // If user has no saved dishes
+            if (!userSavedDishes.length) {
               reject('This user had no saved dishes');
             }
 
@@ -64,9 +74,13 @@ module.exports.getFridgeData = (data) => {
 
             results
               .then(dishArray => {
-                if (dishArray) {
+                console.log(dishArray);
+                if (dishArray.length) {
                   console.log('Users saved dishes aka fridge resolves');
                   resolve(dishArray);
+                } else {
+                  console.error('User has no saved dishes');
+                  reject([]);
                 }
               });
           }); // End of db.UserDish.findAll()

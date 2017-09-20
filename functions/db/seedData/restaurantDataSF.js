913f4e4d81e1836f0475f9574e830fb0b387870b
Lisa Gee <zhusufeng@gmail.com>
2017-09-20 10:43:49 -0700

    Create seed file for new area

---
+++ functions/db/seedData/restaurantDataSF.js
@@ -0,0 +1,86 @@
+module.exports.restaurantDataSF = [
+    {
+      foursquareId: '4af09c87f964a520a1dd21e3',
+      name: 'Tín',
+      phone: '(415) 882-7188',
+      address: 
+        '["937 Howard St (btwn 5th & 6th St)","San Francisco, CA 94103","United States"]',
+      website: 'http://tinsf.com',
+      imageUrl: 
+        '{"id":"50cce7a6e4b04e1f42bde5c5","createdAt":1355605926,"prefix":"https://igx.4sqi.net/img/general/","suffix":"/16156_sy2Bfxv6AAj8gjsa34XmbRGc37dig8pEEFmXR3pbmpE.jpg","width":612,"height":612,"user":{"id":"16156","firstName":"Clement","lastName":"Ng","gender":"male","photo":{"prefix":"https://igx.4sqi.net/img/user/","suffix":"/16156-I0S32BCICN0Q1I34.jpg"}},"visibility":"public"}'
+    },
+    {
+      foursquareId: '49d183b8f964a5208d5b1fe3',
+      name: 'Jasmine Garden',
+      phone: '(415) 861-2682',
+      address: 
+        '["708 14th St (at Church St)","San Francisco, CA 94114","United States"]',
+      website: null,
+      imageUrl: 
+        '{"id":"4f4ac4e3e4b0ccceddbf1f5b","createdAt":1330300131,"prefix":"https://igx.4sqi.net/img/general/","suffix":"/RfcSoYC6OeENhODreADRh9oJkHA1vaGhZGMOmBGzlx8.jpg","width":540,"height":720,"user":{"id":"21295466","firstName":"Erik","lastName":"Tonder","gender":"male","photo":{"prefix":"https://igx.4sqi.net/img/user/","suffix":"/N0QSYRQ5WVRD0FW2.jpg"}},"visibility":"public"}'
+    },
+    {
+      foursquareId: '4ae65f18f964a520bda621e3',
+      name: 'Phở 2000',
+      phone: '(415) 474-1188',
+      address: '["637 Larkin St (btwn Eddy & Ellis)","San Francisco, CA 94109","United States"]',
+      website: 'http://www.pho2000sf.com',
+      imageUrl: '{"id":"510c4feee4b0809a7c3a9178","createdAt":1359761390,"prefix":"https://igx.4sqi.net/img/general/","suffix":"/46537143_Yzw1204Tkg9TRZfr1mrmbPpW4rzWrNSJT2hq3Vhn4MA.jpg","width":720,"height":960,"user":{"id":"46537143","firstName":"Enzo","lastName":"Lombard","gender":"male","photo":{"prefix":"https://igx.4sqi.net/img/user/","suffix":"/GOF5YUNCVDBX1HCW.jpg"}},"visibility":"public"}'
+    },
+    {
+      foursquareId: '505aa37ee4b09d25dc9e7557',
+      name: 'Mau',
+      phone: '(415) 934-8889',
+      address: '["665 Valencia St (18th Street)","San Francisco, CA 94110","United States"]',
+      website: 'http://eatmau.com',
+      imageUrl: '{"id":"50b54609e4b0f32d2fdf25c9","createdAt":1354057225,"prefix":"https://igx.4sqi.net/img/general/","suffix":"/17655754_UeKZ8ZpHkQJmoOqTl2rr8op6yYUDNz1RdkHdXUYGqDc.jpg","width":720,"height":540,"user":{"id":"17655754","firstName":"Arthur","lastName":"Pirogovski","gender":"male","photo":{"prefix":"https://igx.4sqi.net/img/user/","suffix":"/17655754-UNNCCDDGWJYFG0PF.jpg"}},"visibility":"public"}'
+    },
+    {
+      foursquareId: '43f4dc37f964a5209f2f1fe3',
+      name: 'Turtle Tower Restaurant',
+      phone: '(415) 409-3333',
+      address: '["645 Larkin St (btwn Eddy & Ellis)","San Francisco, CA 94109","United States"]',
+      website: 'http://turtletowersf.com',
+      imageUrl: '{"id":"50bfc39ee4b07113ce9f22bc","createdAt":1354744734,"prefix":"https://igx.4sqi.net/img/general/","suffix":"/6339386_dxHE44u0PTnbsdjnsIvHnLeUUvNGMAWgZ76wROzW9FU.jpg","width":540,"height":720,"user":{"id":"6339386","firstName":"Kyle Kohei","gender":"male","photo":{"prefix":"https://igx.4sqi.net/img/user/","suffix":"/6339386-NANPNG5NAVMD5F4R.jpg"}},"visibility":"public"}'
+    },
+    {
+      foursquareId: '4a0665bbf964a520f3721fe3',
+      name: 'Miss SaiGon',
+      phone: '(415) 522-0332',
+      address: '["100 6th St (at Mission St)","San Francisco, CA 94103","United States"]',
+      website: 'http://misssaigonsf.net',
+      imageUrl: '{"id":"50fc6f53e4b02cc6641a903d","createdAt":1358720851,"prefix":"https://igx.4sqi.net/img/general/","suffix":"/747985_JbQyZtI7Us_VZFyL_gFYtFfl2kKbJ7KvhQteaz1eZxw.jpg","width":720,"height":540,"user":{"id":"747985","firstName":"Phil","gender":"male","photo":{"prefix":"https://igx.4sqi.net/img/user/","suffix":"/BJ33O5N4MTXYLTUL.jpg"}},"visibility":"public"}'
+    },
+    {
+      foursquareId: '41799f00f964a520c11d1fe3',
+      name: 'Tú Lan Restaurant',
+      phone: '(415) 626-0927',
+      address: '["8 6th St (btwn Market St & Stevenson St)","San Francisco, CA 94103","United States"]',
+      website: 'http://tulan-vn-restaurant.com',
+      imageUrl: '{"id":"4ddf2ace40a357d4ebf89de9","createdAt":1306471118,"prefix":"https://igx.4sqi.net/img/general/","suffix":"/OBTN0A2ZZSZ24IUR4TM2Y3G5CDJRDY1IKX4GA5UAV4YHFAFL.jpg","width":537,"height":720,"user":{"id":"32937","firstName":"Hana","lastName":"Na","gender":"female","photo":{"prefix":"https://igx.4sqi.net/img/user/","suffix":"/32937-DKJ01IX5HZRFSYPJ.jpg"}},"visibility":"public"}'
+    },
+    {
+      foursquareId: '4ddc50bbd164236dd371b0b7',
+      name: 'Turtle Tower #3',
+      phone: '(415) 904-9888',
+      address: '["501 6th St (at Bryant St)","San Francisco, CA 94103","United States"]',
+      website: 'http://turtletowersf.com',
+      imageUrl: '{"id":"50f5be77e4b0f2332930f39b","createdAt":1358282359,"prefix":"https://igx.4sqi.net/img/general/","suffix":"/32753754_UzB39lsQUTmmfvaccpVEtFAsQJPdMVI2kQZ3RirmEEg.jpg","width":960,"height":720,"user":{"id":"32753754","firstName":"Jessica","gender":"female","photo":{"prefix":"https://igx.4sqi.net/img/user/","suffix":"/LNW502OFRMKAJWCE.jpg"}},"visibility":"public"}'
+    },
+    {
+      foursquareId: '49e1343df964a520a8611fe3',
+      name: 'Phở Tan Hoa',
+      phone: '(415) 673-3163',
+      address: '["431 Jones St (at Steveloe Pl)","San Francisco, CA 94102","United States"]',
+      website: null,
+      imageUrl: '{"id":"52263df711d2cee66498b054","createdAt":1378237943,"prefix":"https://igx.4sqi.net/img/general/","suffix":"/235423_FmeNv3-qpOilA71tgFcd7WozCGGmZOpF2ECYsUNbL_M.jpg","width":960,"height":720,"user":{"id":"235423","firstName":"Stefan","lastName":"Mailvaganam","gender":"male","photo":{"prefix":"https://igx.4sqi.net/img/user/","suffix":"/D0B0NAST5O4UXFEI.jpg"}},"visibility":"public"}'
+    },
+    {
+      foursquareId: '4a8b2acff964a520a00b20e3',
+      name: 'Golden Star Vietnamese Restaurant',
+      phone: '(415) 398-1215',
+      address: '["11 Walter U Lum Pl (Clay)","San Francisco, CA 94108","United States"]',
+      website: null,
+      imageUrl: '{"id":"50383e7ee4b021b33c33e86c","createdAt":1345863294,"prefix":"https://igx.4sqi.net/img/general/","suffix":"/KXNPYVnxl3mVlT0zrHMxjMEk1EvUVKlerQLSdCm_O0o.jpg","width":720,"height":540,"user":{"id":"12810016","firstName":"Sunil","lastName":"Sharma","gender":"male","photo":{"prefix":"https://igx.4sqi.net/img/user/","suffix":"/BURFMGPC0NU51P1Y.jpg"}},"visibility":"public"}'
+    }
+  ];

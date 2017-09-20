8c7e02825550fe6af89d683352b715f0f66e3fc7
Lisa Gee <zhusufeng@gmail.com>
2017-09-20 11:08:05 -0700

    Insert myself as seedData

--- functions/db/seedData/userData.js
+++ functions/db/seedData/userData.js
@@ -1,18 +1,27 @@
 module.exports.userData = [
   {
     userId: 'I8bL1aQKE2cr3jrna6geEpf4bWZ2',
-    userData: '{"user_displayName":"David Kang","user_photoURL":"https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg","user_email":"david.kang714@gmail.com"}'
+    userData: 
+      '{"user_displayName":"David Kang","user_photoURL":"https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg","user_email":"david.kang714@gmail.com"}'
   },
   {
     userId: 'Wzi9EQaXcbgnbyjwWYtVBTvOeN33',
-    userData: '{"user_displayName":"Kevin Su","user_photoURL":"https://lh4.googleusercontent.com/-novUuuOm-vY/AAAAAAAAAAI/AAAAAAAAAFQ/r8Jdpbe6PhI/photo.jpg","user_email":"ksu@ucdavis.edu"}'
+    userData: 
+      '{"user_displayName":"Kevin Su","user_photoURL":"https://lh4.googleusercontent.com/-novUuuOm-vY/AAAAAAAAAAI/AAAAAAAAAFQ/r8Jdpbe6PhI/photo.jpg","user_email":"ksu@ucdavis.edu"}'
   },
   {
     userId: 'drlnGKwWknYALR1rA45apQlF6y13',
-    userData: '{"user_displayName":"Kevin Su","user_photoURL":"https://lh4.googleusercontent.com/-FL0yWop58rE/AAAAAAAAAAI/AAAAAAAALIA/HMGqR06X9Yw/photo.jpg","user_email":"kevinsu1212@gmail.com"}'
+    userData: 
+      '{"user_displayName":"Kevin Su","user_photoURL":"https://lh4.googleusercontent.com/-FL0yWop58rE/AAAAAAAAAAI/AAAAAAAALIA/HMGqR06X9Yw/photo.jpg","user_email":"kevinsu1212@gmail.com"}'
   },
   {
     userId: 'z8GjWJYn9TdY5rKEI4cpsQb9Sdq2',
-    userData: '{"user_displayName":"Chieng Green","user_photoURL":"https://lh3.googleusercontent.com/-tS-NeNOyKaQ/AAAAAAAAAAI/AAAAAAAAABQ/gxLVeDHxaJQ/photo.jpg","user_email":"thund3rkev@gmail.com"}'
+    userData: 
+      '{"user_displayName":"Chieng Green","user_photoURL":"https://lh3.googleusercontent.com/-tS-NeNOyKaQ/AAAAAAAAAAI/AAAAAAAAABQ/gxLVeDHxaJQ/photo.jpg","user_email":"thund3rkev@gmail.com"}'
+  },
+  {
+    userId: 'w2ljLy2P9gNbdh6xF0tVgtTf49u1',
+    userData: 
+      '{"user_displayName":"Lisa G","user_photoURL":"https://lh5.googleusercontent.com/-9NlZmIzUa0U/AAAAAAAAAAI/AAAAAAAAcs4/LhZt3jcoYJk/photo.jpg","user_email":"zhusufeng@gmail.com"}'
   }
 ]

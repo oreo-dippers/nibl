613178b3f4d856e1f32b6881ae497e727c14e305
Ankit09osr <ankit09osr@gmail.com>
2017-09-21 15:51:19 -0700

    Test dummy test to test Jest HTTP request and supertest module

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -16,10 +16,19 @@ functions.config = jest.fn(() => (firebaseAppConfig));
 // Mock the Firebase application
 const myApp = require('../index');
 
-
+const request = require('supertest');
 //Add sum to application and test it
 const sum = require('./../sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+
+describe('Test the test path', () => {
+    test('It should response the GET method', (done) => {
+        request(myApp.app).get('/test').then((response) => {
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+});

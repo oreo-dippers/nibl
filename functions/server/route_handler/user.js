e26c86c1fa99fa39ec9f48103e5a95d0e3419702
Lisa Gee <zhusufeng@gmail.com>
2017-09-21 10:59:56 -0700

    Update statuses to send when catch block executed

--- functions/server/route_handler/user.js
+++ functions/server/route_handler/user.js
@@ -1,14 +1,4 @@
-const querystring = require('querystring');
-const foursquare = require('./../api-config/foursquare-config.js');
-const bodyParser = require('body-parser');
 const utils = require('./../utils/util');
-
-const credentials = {
-  v: '20170904'
-};
-credentials.client_id = foursquare.CLIENT_ID;
-credentials.client_secret = foursquare.CLIENT_SECRET;
-const qs = querystring.stringify(credentials);
 
 module.exports.postUser = (req, res) => {
   utils
@@ -19,7 +9,7 @@ module.exports.postUser = (req, res) => {
     })
     .catch(err => {
       console.error('User Data NOT posted!');
-      res.status(400).send(err);
+      res.status(200).send(err);
     });
 };
 
@@ -32,7 +22,7 @@ module.exports.getFridge = (req, res) => {
     })
     .catch(err => {
       console.error('No fridge data!');
-      res.status(400).send(err);
+      res.status(200).send(err);
     });
 };
 
@@ -45,6 +35,6 @@ module.exports.postFridge = (req, res) => {
     })
     .catch(err => {
       console.error('Fridge data did not post!');
-      res.status(400).send(err);
+      res.status(200).send(err);
     });
 };

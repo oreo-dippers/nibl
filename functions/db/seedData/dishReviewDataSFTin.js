8028f117c021fe6e2110fb41f68dbd22821d48cb
Lisa Gee <zhusufeng@gmail.com>
2017-09-20 17:44:25 -0700

    Create seed Data for dishReviews

---
+++ functions/db/seedData/dishReviewDataSFTin.js
@@ -0,0 +1,23 @@
+module.exports.dishReviewDataSFTin = [
+  {
+    userId: '1',
+    dishId: '1',
+    review: 'These are the best spring rolls!',
+    starRating: '5',
+    imageUrl: 'https://oreo-nibl.appspot.com/new_folder/springroll.jpg'
+  },
+  {
+    userId: '2',
+    dishId: '2',
+    review: 'Pretty tasty and not too oily! Recommend!',
+    starRating: '5',
+    imageUrl: 'https://oreo-nibl.appspot.com/new_folder/chagio.jpg'
+  },
+  {
+    userId: '5',
+    dishId: '3',
+    review: 'Pretty tasty and not too oily! Recommend!',
+    starRating: '5',
+    imageUrl: 'https://oreo-nibl.appspot.com/new_folder/chagio.jpg'
+  }
+];

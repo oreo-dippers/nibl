fae9e70748dd67c55971cdbe06ec606aface4d54
Lisa Gee <zhusufeng@gmail.com>
2017-09-21 11:05:00 -0700

    Add some more seedData for testing

--- functions/db/seedData/userDishData.js
+++ functions/db/seedData/userDishData.js
@@ -10,5 +10,17 @@ module.exports.userDishData = [
   {
     userId: 3,
     dishId: 3
+  },
+  {
+    userId: 5,
+    dishId: 1
+  },
+  {
+    userId: 5,
+    dishId: 2
+  },
+  {
+    userId: 5,
+    dishId: 3
   }
 ];
